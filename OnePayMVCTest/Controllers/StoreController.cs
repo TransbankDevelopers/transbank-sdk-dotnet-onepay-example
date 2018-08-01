c6fb40eb4790a440ad300723e2bbb100f641dbdf
afiebig <alfredo.fiebig@continuum.cl>
2018-07-31 22:03:54 -0400

    Add MVC Test Project

---
+++ OnePayMVCTest/Controllers/StoreController.cs
@@ -0,0 +1,26 @@
+﻿using OnePayMVCTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnePayMVCTest.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            List<Product> cart = new List<Product>();
+            cart.Add(new Product(
+                imagePath: "",
+                name: "Test",
+                price: 10,
+                quantity: 1
+                ));
+            ViewBag.Cart = cart;
+            return View();
+        }
+    }
+}
---
+++ OnePayMVCTest/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace OnePayMVCTest.Models
+{
+    public class Product
+    {
+        public string ImagePath { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string imagePath, string name, int price, int quantity)
+        {
+            ImagePath = imagePath;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
