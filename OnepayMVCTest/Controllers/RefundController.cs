4786360378411e50f76ca1a93f41861e5dd7f075
Gonzalo Castillo <gonzalo.castillo@continuum.cl>
2018-09-28 12:40:44 -0300

    Implement Refund

--- OnepayMVCTest/Controllers/RefundController.cs
+++ OnepayMVCTest/Controllers/RefundController.cs
@@ -1,13 +1,23 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using Transbank.Onepay.Model;
 
 namespace OnePayMVCTest.Controllers
 {
     public class RefundController : Controller
     {
         // GET
-        public ActionResult Create()
+        public ActionResult Create(long amount, string occ, string externalUniqueNumber, string authorizationCode)
         {
-            return View();
+            try
+            {
+                Refund.Create(amount, occ, externalUniqueNumber, authorizationCode);
+                return View();
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("Message", "Error", new {error = e.Message});
+            }
         }
     }
 }
